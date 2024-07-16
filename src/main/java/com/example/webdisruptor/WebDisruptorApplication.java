dac02598f51021834ac36fcd544f55fd2dd2e9e9
= <=>
2024-07-16 15:27:18 +0700

    init

---
+++ src/main/java/com/example/webdisruptor/WebDisruptorApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdisruptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDisruptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDisruptorApplication.class, args);
+    }
+
+}
